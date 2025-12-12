c2f98245e764f69a05d5b83002fb1cd0352f96e7
Maruqes <m4rques1s0n@gmail.com>
2025-12-12 03:10:39 +0000

    dns providers and save

--- master/api/protocol.go
+++ master/api/protocol.go
@@ -1,11 +1,9 @@
 package api
 
 import (
-	"512SvMan/env512"
 	"512SvMan/services"
 	"encoding/json"
 	"net/http"
-	"time"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -15,28 +13,8 @@ func listConnections(w http.ResponseWriter, r *http.Request) {
 	protocolService := services.ProtocolService{}
 	connections := protocolService.GetAllConnections()
 
-	type respConn struct {
-		Addr        string    `json:"addr"`
-		MachineName string    `json:"machineName"`
-		LastSeen    time.Time `json:"lastSeen"`
-		EntryTime   time.Time `json:"entryTime"`
-		Master      bool      `json:"master"`
-	}
-
-	resp := make([]respConn, 0, len(connections))
-	for _, con := range connections {
-		r := respConn{
-			Addr:        con.Addr,
-			MachineName: con.MachineName,
-			LastSeen:    con.LastSeen,
-			EntryTime:   con.EntryTime,
-			Master:      con.Addr == env512.MASTER_INTERNET_IP,
-		}
-		resp = append(resp, r)
-	}
-
 	w.Header().Set("Content-Type", "application/json")
-	_ = json.NewEncoder(w).Encode(resp)
+	_ = json.NewEncoder(w).Encode(connections)
 }
 
 func setupProtocolAPI(r chi.Router) chi.Router {
