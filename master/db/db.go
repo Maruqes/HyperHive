69c22d84221b564b9f4624a65aaa376d6fa71c5c
Marques <m4rques1s0n@gmail.com>
2025-12-15 15:57:40 +0000

    sqlite cons

--- master/db/db.go
+++ master/db/db.go
@@ -12,10 +12,15 @@ var DB *sql.DB
 
 func InitDB(ctx context.Context) {
 	var err error
-	DB, err = sql.Open("sqlite3", "./data.db")
+	// Use WAL to allow concurrent readers during writes, shared cache for multiple pooled connections, and a busy timeout to wait on locks.
+	dsn := "file:data.db?_journal_mode=WAL&_busy_timeout=15000&cache=shared&_foreign_keys=on"
+	DB, err = sql.Open("sqlite3", dsn)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Allow multiple concurrent readers; SQLite will serialize writers in WAL mode.
+	DB.SetMaxOpenConns(8)
+	DB.SetMaxIdleConns(4)
 	if err := DB.PingContext(ctx); err != nil {
 		log.Fatal(err)
 	}
