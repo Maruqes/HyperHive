ddf947b7a0a8975d2e82ee79c99cb2c832ae73f8
Maruqes <m4rques1s0n@gmail.com>
2026-01-02 05:41:29 +0000

    a

--- master/db/db.go
+++ master/db/db.go
@@ -12,8 +12,7 @@ var DB *sql.DB
 
 func InitDB(ctx context.Context) {
 	var err error
-	// Use WAL to allow concurrent readers during writes, shared cache for multiple pooled connections, and a busy timeout to wait on locks.
-	dsn := "file:data.db?_journal_mode=WAL&_busy_timeout=15000&cache=shared&_foreign_keys=on"
+	dsn := "file:data.db?_journal_mode=WAL&_synchronous=NORMAL&_busy_timeout=15000&_foreign_keys=on"
 	DB, err = sql.Open("sqlite3", dsn)
 	if err != nil {
 		log.Fatal(err)
